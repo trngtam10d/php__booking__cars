e7f27c3a6be634edaef946828d649ad22d2f90f7
Tran Ngoc Tam <tran.ngoc.tam@uruca.jp>
2022-07-29 13:17:46 +0700

    update

--- app/Modules/Customer/Controllers/Index.php
+++ app/Modules/Customer/Controllers/Index.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace App\Modules\Customer\Controllers;
 
 use Illuminate\Routing\Controller;
 
 /**
  * Controller Index
- * */ 
+ * */
 class Index extends Controller
 {
     public function index()
